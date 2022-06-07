61d359af4282920c580b3b18948789c5f83a3f7c
Fábio barros Fagundes <fabiobarrosfagundes@gmail.com>
2022-06-07 17:59:52 -0300

    envs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  api_key:'c287e264ac2784b019c70dae2df57c32',
+  url: 'https://api.themoviedb.org/3/'
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,9 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  api_key:'c287e264ac2784b019c70dae2df57c32',
+  url: 'https://api.themoviedb.org/3/'
 };
 
 /*
