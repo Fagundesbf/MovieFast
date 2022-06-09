0a4d9c8eea780d6bec5a4af790ea8bf25dcfc0a2
Fábio Fagundes <fabiobarrosfagundes@gmail.com>
2022-06-09 14:07:24 -0300

    https

--- src/app/models/home/components/trailer/trailer.component.ts
+++ src/app/models/home/components/trailer/trailer.component.ts
@@ -82,7 +82,7 @@ export class TrailerComponent implements OnInit {
   getTrailer(item) {
     this.requestGeneric.get(`${environment.url}movie/` + item.id + `/videos?api_key=` + `${environment.api_key}` + '&page=1').subscribe((resp) => {
       this.subTitleModal = resp['results'][0].name + ' - ' + item.original_language;
-      this.urlTrailerModal = 'http://www.youtube.com/embed/' + resp['results'][0].key + '?autoplay=1&amp;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=nn&amp;modestbranding=1&amp;fs=1&amp;autohide=1';
+      this.urlTrailerModal = 'https://www.youtube.com/embed/' + resp['results'][0].key + '?autoplay=1&amp;origin=https%3A%2F%2Fwww.themoviedb.org&amp;hl=nn&amp;modestbranding=1&amp;fs=1&amp;autohide=1';
     }, (error) => {
       console.error(error)
     });
